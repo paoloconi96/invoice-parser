4ca4833d5b80979d1b3b868c0fa5160462201c2b
Paolo Conizzoli <paolo.conizzoli@gmail.com>
2024-06-26 16:24:09 +0200

    WIP for server logic

---
+++ cmd/api-server/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	idtoken2 "cloud.google.com/go/auth/credentials/idtoken"
+	"context"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/paoloconi96/invoice-parser/internal/invparser"
+	"github.com/paoloconi96/invoice-parser/internal/websocket"
+	"mime/multipart"
+)
+
+type InvoiceRequestBody struct {
+	File *multipart.FileHeader `form:"file"`
+}
+
+var parserChannel = make(chan *invparser.InputInvoice)
+var ctx = context.Background()
+var hub *websocket.Hub
+
+// TODO: Handle err
+var validator, _ = idtoken2.NewValidator(nil)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	//parser := invparser.NewGDocAiParser(
+	//	ctx,
+	//	os.Getenv("GOOGLE_CLOUD_LOCATION"),
+	//	os.Getenv("GOOGLE_CLOUD_PROJECT_ID"),
+	//	os.Getenv("GOOGLE_CLOUD_DOCUMENT_AI_PROCESSOR_ID"),
+	//)
+	parser := invparser.MockParser{}
+	hub = websocket.NewHub()
+	go hub.Run()
+
+	go processInvoices(parserChannel, parser)
+
+	router := gin.Default()
+
+	// TODO: Change this config when the domain gets unified
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowAllOrigins = false
+	corsConfig.AllowOrigins = []string{"http://localhost:3000"}
+	corsConfig.AllowCredentials = true
+	router.Use(cors.New(corsConfig))
+
+	router.POST("/api/v1/invoices", addInvoice)
+	router.GET("/api/v1/websocket", func(ctx *gin.Context) {
+		websocket.HandleWebsocket(ctx, hub)
+	})
+	router.POST("/api/v1/login", login)
+
+	router.Run("localhost:8000")
+}
--- go.mod
+++ go.mod
@@ -36,6 +36,10 @@ require (
 	github.com/google/s2a-go v0.1.7 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.4 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.3.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/kr/text v0.1.0 // indirect
